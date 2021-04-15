62549a3d71fb164646b5b34bb8f5747fe5d1711d
x24git <23037854+x24git@users.noreply.github.com>
2021-04-15 15:07:27 -0700

    Finalize CLI interface to support optional flags and help message

--- EDR-Generator/src/main.rs
+++ EDR-Generator/src/main.rs
@@ -1,35 +1,55 @@
-use crate::modules::process::ProcessManager;
-use crate::modules::common::GenerationError;
-use crate::modules::file_system;
-use crate::modules::network;
-use std::thread;
-use std::time::Duration;
+use clap::{Arg, App};
+use crate::modules::logger::Logger;
+use crate::modules::commander::TaskCommander;
 
 
 mod modules;
 
-fn main()-> Result<(), GenerationError> {
+fn main(){
+    let matches = App::new("EDR Event Generator")
+        .version("1.0")
+        .author("Christopher Makarem")
+        .about("Creates EDR events to verify detection and classification")
+        .arg(Arg::with_name("Deliminator")
+            .short("d")
+            .long("deliminator")
+            .value_name("CHARACTER")
+            .help("Sets a custom deliminator for the specified input file (default value: ',')")
+            .takes_value(true))
+        .arg(Arg::with_name("Output File")
+            .short("o")
+            .long("outfile")
+            .value_name("FILE")
+            .help("Sets the output file location to log events (default value: 'log.csv')")
+            .takes_value(true))
+        .arg(Arg::with_name("INPUT")
+            .value_name("FILE")
+            .help("Sets the input file to use for event creation")
+            .required(true)
+            .index(1))
+        .get_matches();
+    let delim = matches.value_of("Deliminator").unwrap_or(",");
+    let out_file = matches.value_of("Output File").unwrap_or("log.csv");
+    let input_file = matches.value_of("INPUT").unwrap_or("windows_input.csv");
+    let logger = Logger::new(&String::from(out_file));
 
-    let mut process_manager = ProcessManager::new()?;
-    process_manager.new_process(String::from("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"), Some(String::from("google.com msn.com")))?;
-    thread::sleep(Duration::from_millis(1000));
-    let result = process_manager.stop_all()?;
-    println!("{:?}", result);
-    println!("Process Success");
-
-    file_system::new_file(&String::from("test.txt"))?;
-    file_system::mod_file(&String::from("test.txt"))?;
-    file_system::delete_file(&String::from("test.txt"))?;
-    println!("File Success");
-
-    // network::send_message(String::from("127.0.0.1"), 2424, Vec::from(String::from("hello world").as_bytes()))?;
-    network::send_loopback_message( &Vec::from(String::from("hello world").as_bytes()))?;
-    println!("Network Success");
-
-    Ok(())
-
-
-
+    let mut commander = match TaskCommander::new(&input_file.to_string(), delim.as_bytes()[0], logger) {
+        Ok(inner) => inner,
+        Err(e) => {
+            eprintln!("Encountered an unexpected error when setting up: {}", e);
+            return
+        }
+    };
+    let mut commands_processed = 0;
+    while commander.read_next() {
+        commands_processed = commands_processed + 1;
+    }
+    if commands_processed <= 0 {
+        eprintln!("Input File was empty or was of bad format. No Commands Processed")
+    } else {
+        println!("Done. {} Instructions Found. Encountered {} error(s).", commands_processed, commander.get_num_errors())
+    }
+}
 
 
 }
